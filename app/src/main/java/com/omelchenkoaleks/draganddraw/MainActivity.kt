17b2fe4bcebc27db2675975ab52ffbdcbd3b963c
omelchenkoaleks <omelchenkoaleks@gmail.com>
2020-12-25 10:51:41 +0200

    Initial commit

---
+++ app/src/main/java/com/omelchenkoaleks/draganddraw/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.omelchenkoaleks.draganddraw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drag_and_draw)
+    }
+}
